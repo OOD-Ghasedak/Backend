33c3f51e07eb2bc0fc29fc1160b942574a2b4b31
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-23 20:14:32 +0330

    refactor: change name signup serializer

--- accounts/view/signup.py
+++ accounts/view/signup.py
@@ -1,14 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
-from accounts.serializers import GhasedSignupSerializer
+from accounts.serializers import GhasedSignUpSerializer
 from accounts.jwt import get_jwt_token
 
 
 class GhasedSignUpView(APIView):
 
     def post(self, request, *args, **kwargs):
-        serializer = GhasedSignupSerializer(data=request.data)
+        serializer = GhasedSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         account = serializer.save()
         refresh, access = get_jwt_token(account)
