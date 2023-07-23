65a78cf308c44bbc57c631f33039d84a9f3f6a49
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-07-23 18:31:10 +0330

    feat: add new migrations

--- financial/models/exceptions.py
+++
@@ -1,8 +0,0 @@
-from financial.models import Wallet
-
-
-class InvalidBalance(Wallet.Exception):
-    def __init__(self, wallet, balance, actual_balance):
-        super(InvalidBalance, self).__init__(
-            f'wallet {wallet.id} has balance {balance} but it must have {actual_balance}',
-        )
