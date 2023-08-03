e4e36fe7d3075a7ff486cb9233a1341fdc6a2adb
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-08-03 20:35:35 +0330

    feat(verify-signup): add api to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.views import GhasedLoginView, GhasedSignUpView, GhasedProfileView
+from accounts.views import GhasedLoginView, GhasedSignUpView, GhasedProfileView, VerifyForRegisterView
 
 urlpatterns = [
     path('login/', GhasedLoginView.as_view()),
     path('signup/', GhasedSignUpView.as_view()),
+    path('verify-signup/', VerifyForRegisterView.as_view()),
     path('profile/', GhasedProfileView.as_view({
         'get': 'retrieve',
         'patch': 'partial_update',
